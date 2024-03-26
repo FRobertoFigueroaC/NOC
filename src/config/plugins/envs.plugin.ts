e05f9c137e0dcf12f5ca4c3cbd07b9c4e95559f2
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-03-25 20:17:20 -0600

    Adding postgres

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -2,7 +2,9 @@ import 'dotenv/config'
 import * as env from 'env-var'
 
 export const envs = {
+  // General
   PORT: env.get('PORT').required().asPortNumber(),
+  PROD: env.get('PROD').required().asBool(),
   // Mail
   MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
   MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
@@ -12,6 +14,9 @@ export const envs = {
   MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
   MONGO_USER: env.get('MONGO_USER').required().asString(),
   MONGO_PASS: env.get('MONGO_PASS').required().asString(),
-  // Extra
-  PROD: env.get('PROD').required().asBool(),
+  // Postgres DB
+  POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
+  POSTGRES_USER: env.get('POSTGRES_USER').required().asString(),
+  POSTGRES_DB: env.get('POSTGRES_DB').required().asString(),
+  POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').required().asString(),
 }
